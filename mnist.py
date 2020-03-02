b074363533243cc7f533e4db285277eab0e0b773
Arturo Carlos Rodriguez Cobo <art_rg@hotmail.com>
2020-03-02 23:01:54 +0100

    test

--- mnist.py
+++ mnist.py
@@ -2,18 +2,3 @@ import numpy as np
 import pandas as pd
 import matplotlib 
 import matplotlib.pyplot as plt
-
-
-# Data for plotting
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2 * np.pi * t)
-
-fig, ax = plt.subplots()
-ax.plot(t, s)
-
-ax.set(xlabel='time (s)', ylabel='voltage (mV)',
-       title='About as simple as it gets, folks')
-ax.grid()
-
-fig.savefig("test.png")
-plt.show()
