bfd1337a38f51d377bddf03d17ee14d067b7178a
Arturo Carlos Rodriguez Cobo <art_rg@hotmail.com>
2020-03-08 13:33:15 +0100

    pag 100

--- mnist.py
+++ mnist.py
@@ -7,7 +7,8 @@ from sklearn.datasets import fetch_openml
 from sklearn.linear_model import SGDClassifier  
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import precision_score, recall_score, confusion_matrix, precision_recall_curve
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import confusion_matrix, precision_recall_curve, f1_score, roc_curve, roc_auc_score
 mnist = pd.read_csv("mnist_784_csv.csv")
 # print(mnist.head())
 # mnist = fetch_openml("mnist_784",version=1)
@@ -32,14 +33,24 @@ sgd_clf.fit(X_train,y_train_5)
 # accuracy = (result == y_test_5)
 # print(np.sum(accuracy == True)/y_test_5.shape[0])
 # result = cross_val_score(sgd_clf,X_train, y_train_5,cv=3, scoring="accuracy")
-# y_train_pred = cross_val_predict(sgd_clf,X_train, y_train_5,cv=3)
-# print(confusion_matrix(y_train_5,y_train_pred))
-# print("Precision: ",precision_score(y_train_5,y_train_pred))
-# print("Recall: ", recall_score(y_train_5,y_train_pred))
-y_scores = cross_val_predict(sgd_clf,X_train, y_train_5, cv=3, method="decision_function")
-print(y_scores)
-precisions, recalls, thresholds = precision_recall_curve(y_train_5,y_scores)
-plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
-plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
-plt.show()
-# prueba
+y_train_pred = cross_val_predict(sgd_clf,X_train, y_train_5,cv=3)
+print(confusion_matrix(y_train_5,y_train_pred))
+print("Precision: ",precision_score(y_train_5,y_train_pred))
+print("Recall: ", recall_score(y_train_5,y_train_pred))
+print("F1 Score: ", f1_score(y_train_5,y_train_pred))
+y_scores = cross_val_predict(sgd_clf,X_train, y_train_5, cv=3, method="decision_function",n_jobs= 4)
+# pd.set_option('display.max_rows', None)
+# np.set_printoptions(threshold=9999999)
+
+# print(y_scores)
+# precisions, recalls, thresholds = precision_recall_curve(y_train_5,y_scores)
+# print(thresholds)
+# plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
+# plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
+# plt.plot(recalls, precisions, linewidth= 2, label= None)
+# plt.plot([0,1], [0,1], 'k--')
+# fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+# plt.plot(fpr,tpr,linewidth=2,label= None)
+# plt.plot([0,1],[0,1],'k--')
+# plt.show()
+print(roc_auc_score(y_train_5, y_scores))
